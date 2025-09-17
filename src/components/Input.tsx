bf275a36fe31c45ac6170c0c665517fa18d451c8
kwonboryong <kwonboryong@naver.com>
2025-09-17 13:36:36 +0900

    feat: Input 컴포넌트에 Label 추가, props 및 타입 추가

--- src/components/Input.tsx
+++ src/components/Input.tsx
@@ -1,7 +1,28 @@
-export default function Input() {
+import { Input as UIInput } from '@/components/ui/input';
+import { Label } from '@/components/ui/label';
+
+interface InputProps {
+  type: 'email' | 'password' | 'text';
+  placeholder?: string;
+  id?: string;
+  label?: string;
+  className?: string;
+}
+
+export default function Input({
+  type = 'text',
+  placeholder = '',
+  label,
+  id,
+  className = '',
+}: InputProps) {
   return (
-    <div>
-      <input type="text" />
+    <div
+      className={`grid w-full max-w-sm items-center gap-2 mt-3 ${className}`}
+    >
+      {label && <Label htmlFor={id}>{label}</Label>}
+
+      <UIInput type={type} id={id} placeholder={placeholder} />
     </div>
   );
 }
