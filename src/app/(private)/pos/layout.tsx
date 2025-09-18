13ab30b77fe9ecc0bd757711c4f551a572da78bb
kwonboryong <kwonboryong@naver.com>
2025-09-18 17:49:58 +0900

    feat: 페이지 레이아웃 구조 설정

--- src/app/(private)/pos/layout.tsx
+++ src/app/(private)/pos/layout.tsx
@@ -1,11 +1,17 @@
+import BackButton from '@/components/BackButton';
 import Header from '@/components/Header';
 
 export default function Layout({ children }: { children: React.ReactNode }) {
   return (
-    <div className="bg-[#F5F5F5] h-screen">
+    <div className="bg-default flex flex-col min-h-screen">
       <Header storeName={'/'} />
 
-      {children}
+      <main className="w-[80%]  mx-auto pr-[80px] pt-8  flex-1 overflow-auto">
+        <div className="flex items-center gap-4 mb-6">
+          <BackButton />
+          <div className="flex-1">{children}</div>
+        </div>
+      </main>
     </div>
   );
 }
