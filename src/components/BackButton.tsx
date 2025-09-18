5d74ad5c2dac41f0e8e0d56acbaa1de11ae9accc
kwonboryong <kwonboryong@naver.com>
2025-09-18 16:43:15 +0900

    feat: BackButton 컴포넌트 - 아이콘, 뒤로가기 기능 추가

---
+++ src/components/BackButton.tsx
@@ -0,0 +1,20 @@
+'use client';
+import { ChevronLeft } from 'lucide-react';
+import { Button } from './ui/button';
+import { useRouter } from 'next/navigation';
+
+export default function BackButton() {
+  const router = useRouter();
+
+  return (
+    <Button
+      type="button"
+      variant={'ghost'}
+      className="w-14"
+      onClick={() => router.back()}
+      aria-label="뒤로가기"
+    >
+      <ChevronLeft className="size-5" aria-hidden />
+    </Button>
+  );
+}
