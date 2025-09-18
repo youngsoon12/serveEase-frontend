f2b441bb2d5809964964100d08b4df1bb98cf169
kwonboryong <kwonboryong@naver.com>
2025-09-18 16:24:43 +0900

    style: 페이지 레이아웃에 Header 추가

--- src/app/(private)/pos/layout.tsx
+++ src/app/(private)/pos/layout.tsx
@@ -1,3 +1,11 @@
+import Header from '@/components/Header';
+
 export default function Layout({ children }: { children: React.ReactNode }) {
-  return <div>{children}</div>;
+  return (
+    <div className="bg-[#F5F5F5] h-screen">
+      <Header storeName={'/'} />
+
+      {children}
+    </div>
+  );
 }
