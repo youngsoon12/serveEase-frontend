6c91fb3e22149e1e1adff563ca2408526d6efd1e
youngsoon12 <enejwk789@gmail.com>
2025-09-20 18:32:40 +0900

    fix: button 및 input 길이 통일 및 수정

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -33,7 +33,7 @@ export default function Home() {
         {/** 왼쪽 로고 박스 */}
         <div className="w-full h-full">
           <div className="absolute bottom-32 left-20 z-10 ">
-            <h1 className="text-white text-5xl font-bold">
+            <h1 className="text-white text-3xl md:text-5xl font-bold">
               빠르고 직관적인 매장 결제
             </h1>
             <Logo size="lg" color="landing" />
@@ -43,7 +43,7 @@ export default function Home() {
         {/** 오른쪽 로그인 박스*/}
         <div className="w-full flex justify-center items-center mr-5">
           <div
-            className="w-105 h-101 flex flex-col items-center border-gray bg-white p-4 rounded-[24px]"
+            className="w-[clamp(20rem,40vw,28rem)] h-[clamp(18rem,50vh,26rem)] flex flex-col items-center border-gray bg-white p-4 rounded-[24px]"
             style={{ boxShadow: '0px 8px 12px rgba(0,0,0,0.25)' }}
           >
             <h1 className="text-black text-[40px] font-bold text-center mt-5 mb-5">
@@ -63,12 +63,20 @@ export default function Home() {
               name="pwd"
               onChange={handleUserInfoChange}
             />
-            <Link href="/signup" className="self-end px-0 mt-2">
-              <Button variant="link" className="self-end px-0 mt-2">
-                회원가입
-              </Button>
-            </Link>
-            <Button variant="default" className="w-full h-12 mt-2 mx-auto">
+            <div className="w-full max-w-sm flex justify-end">
+              <Link href="/signup" className="self-end px-0 mt-2">
+                <Button
+                  variant="link"
+                  className="px-0 mt-2 text-[#BBBBBB] hover:text-black transition-colors"
+                >
+                  회원가입
+                </Button>
+              </Link>
+            </div>
+            <Button
+              variant="default"
+              className="w-full max-w-sm h-12 mt-2 mx-auto"
+            >
               로그인
             </Button>
           </div>
