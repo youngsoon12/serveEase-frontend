9d68d01869d8dfee7dd68e331b0fb2a7f284407a
kwonboryong <kwonboryong@naver.com>
2025-09-21 14:07:13 +0900

    feat: 테이블 페이지의 테이블 개수 수정 모달 - ModalShell 컴포넌트로 UI 구성

--- src/app/(private)/pos/(shell-layout)/tables/@modal/(.)setup/page.tsx
+++ src/app/(private)/pos/(shell-layout)/tables/@modal/(.)setup/page.tsx
@@ -1,12 +1,23 @@
 'use client';
 
+import Input from '@/components/Input';
 import ModalShell from '@/components/ModalShell';
+import { Button } from '@/components/ui/button';
 
 export default function TableSetupModal() {
   return (
     <>
-      <div>테이블 갯수 수정 모달</div>
-      <ModalShell></ModalShell>
+      <ModalShell title={'테이블 설정'} size="sm">
+        <div>
+          <p className="text-lg">테이블 개수</p>
+          <div className="flex justify-center">
+            <div className="flex flex-col gap-4  w-72">
+              <Input type={'text'} className="w-full h-11" />
+              <Button className="w-full h-11">저장</Button>
+            </div>
+          </div>
+        </div>
+      </ModalShell>
     </>
   );
 }
