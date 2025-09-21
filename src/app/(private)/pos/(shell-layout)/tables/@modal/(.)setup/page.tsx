01ab302522ad8c59e7c5af8c24007c2e52a46014
kwonboryong <kwonboryong@naver.com>
2025-09-21 11:48:10 +0900

    init: 테이블 페이지의 테이블 개수 수정 모달 페이지, 원본 페이지 생성

---
+++ src/app/(private)/pos/(shell-layout)/tables/@modal/(.)setup/page.tsx
@@ -0,0 +1,12 @@
+'use client';
+
+import ModalShell from '@/components/ModalShell';
+
+export default function TableSetupModal() {
+  return (
+    <>
+      <div>테이블 갯수 수정 모달</div>
+      <ModalShell></ModalShell>
+    </>
+  );
+}
---
+++ src/app/(private)/pos/(shell-layout)/tables/setup/page.tsx
@@ -0,0 +1,12 @@
+'use client';
+
+import ModalShell from '@/components/ModalShell';
+
+export default function TableSetupPage() {
+  return (
+    <div>
+      테이블 갯수 수정 원본 페이지
+      <ModalShell />
+    </div>
+  );
+}
