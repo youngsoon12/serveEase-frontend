1195dda125e0896d0f25c298629b6e1ecdd41477
kwonboryong <kwonboryong@naver.com>
2025-09-21 14:09:33 +0900

    fix: 테이블 개수 수정 원본 페이지 수정

--- src/app/(private)/pos/(shell-layout)/tables/setup/page.tsx
+++ src/app/(private)/pos/(shell-layout)/tables/setup/page.tsx
@@ -1,12 +1,5 @@
 'use client';
 
-import ModalShell from '@/components/ModalShell';
-
 export default function TableSetupPage() {
-  return (
-    <div>
-      테이블 갯수 수정 원본 페이지
-      <ModalShell />
-    </div>
-  );
+  return <div>테이블 갯수 수정 원본 페이지</div>;
 }
