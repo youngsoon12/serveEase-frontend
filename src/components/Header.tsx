7257a3ad615f0f90b15c3d96ae6ce64ee68becdf
kwonboryong <kwonboryong@naver.com>
2025-09-23 09:36:59 +0900

    fix: 기존 고정 시간 -> 실시간 시간 갱신 로직으로 대체, 시간 포맷 코드를 LiveTime 컴포넌트로 분리

--- src/components/Header.tsx
+++ src/components/Header.tsx
@@ -1,20 +1,12 @@
 import Link from 'next/link';
 import Logo from './Logo';
+import LiveTime from './LiveTime';
 
 interface HeaderProps {
   storeName: string;
 }
 
 export default function Header({ storeName = '매장명' }: HeaderProps) {
-  const currentTime =
-    new Date()
-      .toLocaleTimeString('ko-KR', {
-        hour: '2-digit',
-        minute: '2-digit',
-        hour12: true,
-      })
-      .replace(':', '시 ') + '분';
-
   return (
     <header className="h-20 bg-header text-white">
       <div className="mx-auto  h-full flex items-center justify-between px-16">
@@ -23,7 +15,9 @@ export default function Header({ storeName = '매장명' }: HeaderProps) {
         </Link>
         <div className="flex items-center gap-5">
           <p>{`${storeName}님 안녕하세요.`}</p>
-          <p className="font-light text-gray-300">{currentTime}</p>
+          <p className="font-light text-gray-300">
+            <LiveTime />
+          </p>
         </div>
       </div>
     </header>
