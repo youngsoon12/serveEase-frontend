07be8fc3441fdbf48279fb6e72449b9481e98f18
kwonboryong <kwonboryong@naver.com>
2025-09-23 15:16:40 +0900

    chore: Next.js 프록시 설정

--- next.config.ts
+++ next.config.ts
@@ -1,5 +1,14 @@
 import type { NextConfig } from 'next';
 
-const nextConfig: NextConfig = {};
+const nextConfig: NextConfig = {
+  async rewrites() {
+    return [
+      {
+        source: '/api/:path*',
+        destination: 'http://3.39.230.117:8080/api/:path*',
+      },
+    ];
+  },
+};
 
 export default nextConfig;
