10b6536b539b4ad6bdb993a35d31576c22574049
kwonboryong <kwonboryong@naver.com>
2025-09-26 14:48:15 +0900

    fix: 카테고리 타입명 수정

--- src/hooks/useCategories.ts
+++ src/hooks/useCategories.ts
@@ -1,8 +1,8 @@
-import { CategoriesResponse, getCategories } from '@/app/api/pos';
+import { Category, getCategories } from '@/app/api/pos';
 import { useQuery } from '@tanstack/react-query';
 
 export default function useCategories() {
-  const query = useQuery<CategoriesResponse[]>({
+  const query = useQuery<Category[]>({
     queryKey: ['categories'],
     queryFn: getCategories,
     staleTime: 5 * 50 * 1000,
