2b3f4a4b219648e6e42582cfac347166e73c4506
kwonboryong <kwonboryong@naver.com>
2025-09-27 09:46:24 +0900

    feat: 주문 POST 쿼리 함수 생성

---
+++ src/hooks/useOrder.ts
@@ -0,0 +1,28 @@
+import {
+  CreateOrderResponse,
+  CreateOrderRequest,
+  createOrder,
+} from '@/app/api/order';
+import { useMutation, useQueryClient } from '@tanstack/react-query';
+import { useRouter } from 'next/navigation';
+import { toast } from 'sonner';
+
+export function useCreateOrder() {
+  const router = useRouter();
+  const queryClient = useQueryClient();
+
+  return useMutation<CreateOrderResponse, unknown, CreateOrderRequest>({
+    mutationFn: (payload) => createOrder(payload),
+    onSuccess: (order) => {
+      toast.success('주문이 접수되었습니다.');
+      console.log(order);
+
+      router.push('/pos/tables');
+      queryClient.invalidateQueries({ queryKey: ['tables'] });
+    },
+    onError: (error) => {
+      toast.error('주문 처리에 실패했습니다.');
+      console.log(error);
+    },
+  });
+}
