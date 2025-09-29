5f9ab1c01c43ac6230f7c9aa37076aa46718ccab
kwonboryong <kwonboryong@naver.com>
2025-09-29 15:49:05 +0900

    feat: 재주문 api 쿼리 함수 생성

--- src/hooks/useOrder.ts
+++ src/hooks/useOrder.ts
@@ -3,6 +3,7 @@ import {
   OrderRequest,
   createOrder,
   getOrder,
+  addOrder,
 } from '@/app/api/order';
 import { useMutation, useQuery, useQueryClient } from '@tanstack/react-query';
 import { useRouter } from 'next/navigation';
@@ -30,6 +31,29 @@ export function useCreateOrder(tableId: number) {
 
       router.push('/pos/tables');
       queryClient.invalidateQueries({ queryKey: ['tables'] });
+    },
+    onError: (err) => {
+      toast.error('주문 처리에 실패했습니다.');
+
+      console.error('status:', err?.response?.status);
+      console.error('data:', err?.response?.data);
+    },
+  });
+}
+
+export function useAddOrder(orderId: number) {
+  const router = useRouter();
+  const queryClient = useQueryClient();
+
+  return useMutation<OrderResponse, AxiosError<ApiErrorBody>, OrderRequest>({
+    mutationFn: (payload) => addOrder(orderId, payload),
+    onSuccess: (order) => {
+      toast.success('주문이 접수되었습니다.');
+      console.log(order);
+
+      router.push('/pos/tables');
+      queryClient.invalidateQueries({ queryKey: ['tables'] });
+      queryClient.invalidateQueries({ queryKey: ['order', orderId] });
     },
     onError: (err) => {
       toast.error('주문 처리에 실패했습니다.');
