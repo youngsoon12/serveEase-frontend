50be37967f115f7beb61b8643d4dda05f9982002
youngsoon12 <enejwk789@gmail.com>
2025-10-01 06:18:18 +0900

    fix : ModalShell 임시 수정 (title, size, {Children}이 존재 하지 않아 빌드시 에러 발생 임시로 내용 보강 추후 수정 필요)

--- src/app/(private)/pos/tables/[tableId]/orders/page.tsx
+++ src/app/(private)/pos/tables/[tableId]/orders/page.tsx
@@ -6,7 +6,10 @@ export default function OrderCheckPage() {
   return (
     <div>
       주문 내역 확인 원본 페이지
-      <ModalShell />
+      {/* ModalShell이 title, size, {children} 을 필수로 받아 임시로 수정해놨습니다. */}
+      <ModalShell title="주문 내역 확인" size="lg">
+        <div>빌드를 위한 임시 영역</div>
+      </ModalShell>
     </div>
   );
 }
