26533b42284e67bdd38012eac3e536cad5f11305
kwonboryong <kwonboryong@naver.com>
2025-10-02 16:38:51 +0900

    refactor: 주문 내역 확인 - 모달 & 원본 페이지 공통 컴포넌트로 대체

--- src/app/(private)/pos/tables/[tableId]/orders/page.tsx
+++ src/app/(private)/pos/tables/[tableId]/orders/page.tsx
@@ -1,12 +1,19 @@
 'use client';
 
-import ModalShell from '@/components/ModalShell';
+import BackButton from '@/components/BackButton';
+import OrderCheck from '@/components/page/OrderCheck';
 
 export default function OrderCheckPage() {
   return (
-    <div>
-      주문 내역 확인 원본 페이지
-      <ModalShell />
+    <div className="w-[80%] mx-auto pt-8 flex-1 overflow-auto">
+      <BackButton buttonStyle={'w-14'} iconStyle={'size-5'} />
+      <main className="flex flex-col items-center justify-center p-5">
+        <h1 className="text-2xl font-bold mb-8">주문 내역 확인</h1>
+
+        <div className="bg-white p-8 rounded-lg shadow-lg border w-full max-w-sm">
+          <OrderCheck />
+        </div>
+      </main>
     </div>
   );
 }
