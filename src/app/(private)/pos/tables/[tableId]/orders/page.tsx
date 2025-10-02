dc17d26a0649182a3c4e349fbcb6ab9a9934cd00
kwonboryong <kwonboryong@naver.com>
2025-10-02 16:45:28 +0900

    feat: 뒤로가기 컴포넌트로 테이블 페이지로 이동 경로 추가

--- src/app/(private)/pos/tables/[tableId]/orders/page.tsx
+++ src/app/(private)/pos/tables/[tableId]/orders/page.tsx
@@ -6,7 +6,11 @@ import OrderCheck from '@/components/page/OrderCheck';
 export default function OrderCheckPage() {
   return (
     <div className="w-[80%] mx-auto pt-8 flex-1 overflow-auto">
-      <BackButton buttonStyle={'w-14'} iconStyle={'size-5'} />
+      <BackButton
+        buttonStyle={'w-14'}
+        iconStyle={'size-5'}
+        targetPath={'/pos/tables'}
+      />
       <main className="flex flex-col items-center justify-center p-5">
         <h1 className="text-2xl font-bold mb-8">주문 내역 확인</h1>
 
