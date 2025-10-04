eef0a64b12360398469d0f69ba28c6840889ce9a
kwonboryong <kwonboryong@naver.com>
2025-10-04 16:24:46 +0900

    feat: 토스페이먼츠 api 사용 - 1. api 키 설정 - 2. SDK 초기화 - 3. 결제창 메서드 연결 및 옵션 설정(amount, orderName, url 등)

--- src/components/page/OrderCheck.tsx
+++ src/components/page/OrderCheck.tsx
@@ -1,16 +1,80 @@
 'use client';
 
+import {
+  loadTossPayments,
+  TossPaymentsPayment,
+} from '@tosspayments/tosspayments-sdk';
 import { useOrder } from '@/hooks/useOrder';
 import { useSearchParams } from 'next/navigation';
 import { Button } from '@/components/ui/button';
+import { useEffect, useState } from 'react';
+
+const clientKey = process.env.NEXT_PUBLIC_TOSS_CLIENT_KEY!;
+
+const origin = window.location.origin;
 
 export default function OrderCheck() {
+  // 주문 내역
   const param = useSearchParams();
   const orderIdParam = param.get('orderId');
 
   const orderId = orderIdParam ? Number(orderIdParam) : undefined;
 
   const { data } = useOrder(orderId);
+
+  console.log(data);
+
+  // 토스페이먼츠 결제 api
+  const [paymentInstance, setPaymentInstance] =
+    useState<TossPaymentsPayment | null>(null);
+
+  useEffect(() => {
+    if (!clientKey || !data) return;
+
+    // 회원 식별 값
+    const customerKey = String(data?.id);
+
+    // SDK 초기화
+    const initializeTossPayments = async () => {
+      try {
+        const tossPayments = await loadTossPayments(clientKey);
+        const payment = tossPayments.payment({ customerKey: customerKey });
+
+        setPaymentInstance(payment);
+      } catch (error) {
+        console.error('토스 SDK 초기화 실패:', error);
+      }
+    };
+
+    initializeTossPayments();
+  }, [data]);
+
+  // 결제창 띄우기
+  const handlePayClick = async () => {
+    if (!orderId || !data || !paymentInstance) return;
+
+    // orderName 생성
+    const firstItemName = data.orderItems[0].menuName;
+    const otherItemsCount = data.orderItems.length - 1;
+    const orderName =
+      otherItemsCount > 0
+        ? `${firstItemName} 외 ${otherItemsCount}건`
+        : firstItemName;
+
+    console.log(orderName); // 콤비네이션 피자 외 3건
+
+    await paymentInstance.requestPayment({
+      method: 'CARD',
+      amount: {
+        currency: 'KRW',
+        value: data.totalPrice,
+      },
+      orderId: 'ord_25mb3kz5_ab12cd', // 결제용 orderId 필요(영문 대소문자, 숫자, 특수문자(-, _) 만 허용,6자 ~ 64자)
+      orderName: orderName,
+      successUrl: `${origin}/pos/payment/success`,
+      failUrl: `${origin}/pos/payment/fail`,
+    });
+  };
 
   return (
     <div className="flex flex-col w-[21rem] max-w-full">
@@ -43,7 +107,9 @@ export default function OrderCheck() {
               <span>총 결제 금액</span>
               <span>{data?.totalPrice.toLocaleString()}원</span>
             </div>
-            <Button className="w-full h-12">결제하기</Button>
+            <Button className="w-full h-12" onClick={handlePayClick}>
+              결제하기
+            </Button>
           </div>
         </>
       )}
