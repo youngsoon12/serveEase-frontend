ae280b41e2984f6989a20ffdd771795866ea01d4
kwonboryong <kwonboryong@naver.com>
2025-10-07 13:57:51 +0900

    delete: bundle-analyzer 삭제

--- next.config.ts
+++ next.config.ts
@@ -1,9 +1,4 @@
 import type { NextConfig } from 'next';
-import bundleAnalyzer from '@next/bundle-analyzer';
-
-const withBundleAnalyzer = bundleAnalyzer({
-  enabled: process.env.ANALYZE === 'true',
-});
 
 const nextConfig: NextConfig = {
   async rewrites() {
@@ -16,4 +11,4 @@ const nextConfig: NextConfig = {
   },
 };
 
-export default withBundleAnalyzer(nextConfig);
+export default nextConfig;
