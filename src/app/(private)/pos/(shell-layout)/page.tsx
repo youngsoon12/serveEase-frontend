b17e94745446808ec9c7b31229d447347e1c3d95
kwonboryong <kwonboryong@naver.com>
2025-10-08 15:26:53 +0900

    refactor: 포스 모드 페이지 접근성 리팩토링 - h1 페이지 제목 추가 - 페이지명 명시적으로 수정(page → PosModePage) - ModeCard 구조를 `<ul>`, `<li>` 태그로 시맨틱하게 개선

--- src/app/(private)/pos/(shell-layout)/page.tsx
+++ src/app/(private)/pos/(shell-layout)/page.tsx
@@ -1,19 +1,23 @@
 import ModeCard from '@/components/ModeCard';
 import React from 'react';
 
-const page = () => {
+export default function PosModePage() {
   return (
     <div className="h-full flex items-center justify-center ">
-      <div className="flex gap-6 ">
-        <ModeCard href={'/pos/tables'} content={'테이블'} />
-        <ModeCard
-          href={'/pos/products'}
-          content={'상품 관리'}
-          variant="product"
-        />
-      </div>
+      <h1 className="sr-only">POS 모드 선택</h1>
+
+      <ul className="flex gap-6 ">
+        <li>
+          <ModeCard href={'/pos/tables'} content={'테이블'} />
+        </li>
+        <li>
+          <ModeCard
+            href={'/pos/products'}
+            content={'상품 관리'}
+            variant="product"
+          />
+        </li>
+      </ul>
     </div>
   );
-};
-
-export default page;
+}
