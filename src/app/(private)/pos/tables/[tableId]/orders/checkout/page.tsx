ad5ceb95fe8f3a1284a3aafc7401346f5a2ed4f3
youngsoon12 <enejwk789@gmail.com>
2025-10-17 05:57:37 +0900

    feat: 기존 Confirm Modal의 값을 기본 값으로 설정하고 confirmButtonClassName, cancelButtonClassName 속성 추가

--- src/app/(private)/pos/tables/[tableId]/orders/checkout/page.tsx
+++ src/app/(private)/pos/tables/[tableId]/orders/checkout/page.tsx
@@ -1,3 +1,31 @@
+'use client';
+import ConfirmModal from '@/components/ConfirmModal';
+import { useState } from 'react';
+
 export default function checkout() {
-  return <div>체크아웃</div>;
+  const [open, setOpen] = useState(false);
+  const handleLogout = () => {
+    console.log('히원탈되');
+  };
+  return (
+    <div>
+      <button
+        onClick={() => {
+          setOpen((prev) => !prev);
+        }}
+      >
+        클릭
+      </button>
+      <ConfirmModal
+        open={open}
+        onOpenChange={setOpen}
+        title="현금 결제하시겠습니까?"
+        confirmText="현금 결제"
+        cancelText="아니오"
+        confirmButtonClassName="bg-blue-600 hover:bg-blue-700 text-white sm:w-36 sm:h-12 text-lg"
+        cancelButtonClassName="border border-gray-300 sm:w-28 sm:h-12 text-gray-700"
+        onConfirm={handleLogout}
+      />
+    </div>
+  );
 }
--- src/components/ui/ConfirmModal.tsx
+++
@@ -1,5 +0,0 @@
-function ConfirmModal() {
-  return <div>ConfirmModal</div>;
-}
-
-export default ConfirmModal;
