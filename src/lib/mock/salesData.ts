ceac3b0b86cead294fd217a1c1c61ebf284ea478
kwonboryong <kwonboryong@naver.com>
2025-10-17 16:13:20 +0900

    feat: KPI 카드 컴포넌트에서 사용되는 목 데이터 분리

---
+++ src/lib/mock/salesData.ts
@@ -0,0 +1,18 @@
+// SalesKPICard
+export const mockData = {
+  실매출: {
+    day: 1234567,
+    week: 8500000,
+    month: 35000000,
+  },
+  '주문건당 평균가': {
+    day: 25000,
+    week: 27000,
+    month: 26500,
+  },
+  '취소 금액': {
+    day: 50000,
+    week: 350000,
+    month: 1200000,
+  },
+};
