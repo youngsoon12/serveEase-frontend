a3c14313b1979060c4ab359bdb167a4b7628ffdf
kwonboryong <kwonboryong@naver.com>
2025-10-20 14:47:48 +0900

    feat: 결제 내역 카드 컴포넌트의 열 분리

--- src/components/page/payment-history/PaymentDetailCard.tsx
+++ src/components/page/payment-history/PaymentDetailCard.tsx
@@ -1,18 +1,5 @@
 import { PaymentDetail } from '@/types/payment-history';
-
-interface DetailRowProps {
-  label: string;
-  value: string;
-}
-
-function DetailRow({ label, value }: DetailRowProps) {
-  return (
-    <div className="flex justify-between items-center">
-      <span className="text-sm text-gray-600">{label}</span>
-      <span className="text-sm text-gray-900">{value}</span>
-    </div>
-  );
-}
+import DetailRow from './DetailRow';
 
 interface PaymentDetailCardProps {
   detail: PaymentDetail;
