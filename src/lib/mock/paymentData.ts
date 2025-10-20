6b0a6a6267197bbb92fd9e5daff6828bf7707ec2
kwonboryong <kwonboryong@naver.com>
2025-10-20 14:33:51 +0900

    feat: 결제 상세 내역에 필요한 목 데이터 추가

--- src/lib/mock/paymentData.ts
+++ src/lib/mock/paymentData.ts
@@ -1,4 +1,4 @@
-import { Payment } from '@/types/payment-history';
+import { OrderDetail, Payment, PaymentDetail } from '@/types/payment-history';
 
 // PaymentList
 export const mockPaymentData: Payment[] = [
@@ -41,5 +41,151 @@ export const mockPaymentData: Payment[] = [
     status: '완료',
     description: 'Ice 아메리카노 등/총2건',
     date: '1016번 02-02 11:00',
+  },
+];
+
+// 결제 상세 정보 목 데이터
+export const mockPaymentDetailData: PaymentDetail[] = [
+  {
+    paymentId: 1,
+    totalAmount: 30000,
+    category: '신용카드',
+    totalPrice: 27273,
+    vat: 2727,
+    paymentTime: '2024-10-20 15:24',
+    paymentMethod: '삼성카드 613*',
+    approvalNumber: '115679435',
+    approvalStatus: '결제완료',
+  },
+  {
+    paymentId: 2,
+    totalAmount: 52000,
+    category: '신용카드',
+    totalPrice: 47273,
+    vat: 4727,
+    paymentTime: '2024-10-19 15:23',
+    paymentMethod: '현대카드 112*',
+    approvalNumber: '115679436',
+    approvalStatus: '취소완료',
+  },
+  {
+    paymentId: 3,
+    totalAmount: 12500,
+    category: '신용카드',
+    totalPrice: 11364,
+    vat: 1136,
+    paymentTime: '2024-10-18 15:22',
+    paymentMethod: '신한카드 332*',
+    approvalNumber: '115679437',
+    approvalStatus: '결제완료',
+  },
+  {
+    paymentId: 4,
+    totalAmount: 5000,
+    category: '계좌이체(현금 영수증)',
+    totalPrice: 4545,
+    vat: 455,
+    paymentTime: '2024-02-02 13:40',
+    paymentMethod: '계좌이체(현금 영수증)',
+    approvalNumber: '115679438',
+    approvalStatus: '결제완료',
+  },
+  {
+    paymentId: 5,
+    totalAmount: 12500,
+    category: '신용카드',
+    totalPrice: 11364,
+    vat: 1136,
+    paymentTime: '2024-02-02 11:00',
+    paymentMethod: '현대카드 112*',
+    approvalNumber: '115679439',
+    approvalStatus: '결제완료',
+  },
+];
+
+// 주문 상세 정보 목 데이터
+export const mockOrderDetailData: OrderDetail[] = [
+  {
+    paymentId: 1,
+    items: [
+      {
+        id: 1,
+        name: 'Hot 아메리카노',
+        quantity: 1,
+        price: 4500,
+        options: '샷추가(500)/사이즈업(500)',
+      },
+      {
+        id: 2,
+        name: '카페라떼',
+        quantity: 1,
+        price: 5000,
+      },
+    ],
+  },
+  {
+    paymentId: 2,
+    items: [
+      {
+        id: 3,
+        name: '아메리카노',
+        quantity: 2,
+        price: 9000,
+        options: 'Ice',
+      },
+      {
+        id: 4,
+        name: '카페모카',
+        quantity: 1,
+        price: 5500,
+      },
+    ],
+  },
+  {
+    paymentId: 3,
+    items: [
+      {
+        id: 5,
+        name: 'Hot 아메리카노',
+        quantity: 2,
+        price: 9000,
+      },
+      {
+        id: 6,
+        name: '바닐라라떼',
+        quantity: 1,
+        price: 5000,
+        options: '시럽추가(500)',
+      },
+    ],
+  },
+  {
+    paymentId: 4,
+    items: [
+      {
+        id: 7,
+        name: '대추생강차',
+        quantity: 1,
+        price: 5000,
+      },
+    ],
+  },
+  {
+    paymentId: 5,
+    items: [
+      {
+        id: 8,
+        name: 'Ice 아메리카노',
+        quantity: 2,
+        price: 9000,
+      },
+      {
+        id: 9,
+        name: '헤이즐넛라떼',
+        quantity: 1,
+        price: 5500,
+        options: '휘핑크림추가(500)',
+      },
+    ],
   },
 ];
