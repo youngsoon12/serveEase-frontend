590fce9ca20d1a3184799f9437edd7c71e312f48
kwonboryong <kwonboryong@naver.com>
2025-10-29 14:02:26 +0900

    feat: 매출 리포트 패칭 리액트 쿼리 생성

---
+++ src/hooks/useSalesReport.ts
@@ -0,0 +1,21 @@
+import {
+  getSalesReport,
+  SalesReportParams,
+  SalesReportResponse,
+} from '@/app/api/salesReport';
+import { useQuery } from '@tanstack/react-query';
+
+export function useSalesReport(params: SalesReportParams) {
+  return useQuery<SalesReportResponse>({
+    queryKey: [
+      'salesReport',
+      params.to,
+      params.from,
+      params.storeId,
+      params.period,
+      params,
+    ],
+    queryFn: () => getSalesReport(params),
+    staleTime: 60 * 5 * 1000,
+  });
+}
