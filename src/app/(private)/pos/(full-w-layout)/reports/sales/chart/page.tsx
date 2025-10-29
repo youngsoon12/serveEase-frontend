b41be310812b983b4e0dcd63d6ad320c13fa2694
kwonboryong <kwonboryong@naver.com>
2025-10-29 14:27:34 +0900

    feat: 당일 기준 날짜 계산 & 매출 현황 쿼리 함수 호출

--- src/app/(private)/pos/(full-w-layout)/reports/sales/chart/page.tsx
+++ src/app/(private)/pos/(full-w-layout)/reports/sales/chart/page.tsx
@@ -4,11 +4,25 @@ import DateRangePicker from '@/components/page/sales-chart/DateRangePicker';
 import SalesChart from '@/components/page/sales-chart/SalesChart';
 import SalesKPICard from '@/components/page/sales-chart/SalesKPICard';
 import SalesPeriodTabs from '@/components/page/sales-chart/SalesPeriodTabs';
+import salesCalculateDate from '@/lib/salesCalculateDate';
 import { Period } from '@/types/sales';
+import { getStoreId } from '@/app/api/store';
+import { useSalesReport } from '@/hooks/useSalesReport';
 import { useState } from 'react';
 
 export default function SalesReportChart() {
   const [period, setPeriod] = useState<Period>('day');
+
+  const storeId = getStoreId();
+
+  const { to, from } = salesCalculateDate(period);
+
+  const { data: salesData } = useSalesReport({
+    to,
+    from,
+    storeId,
+    period,
+  });
 
   return (
     <div className="p-6 space-y-6 ">
@@ -21,12 +35,18 @@ export default function SalesReportChart() {
       </div>
 
       <div className="grid grid-cols-3 gap-4">
-        <SalesKPICard title="실매출" period={period} />
-        <SalesKPICard title="주문건당 평균가" period={period} />
-        <SalesKPICard title="취소 금액" period={period} />
+        <SalesKPICard title="실매출" value={salesData?.summary.netSales ?? 0} />
+        <SalesKPICard
+          title="주문건당 평균가"
+          value={salesData?.summary.averageOrderValue ?? 0}
+        />
+        <SalesKPICard
+          title="취소 금액"
+          value={salesData?.summary.canceledAmount ?? 0}
+        />
       </div>
 
-      <SalesChart period={period} />
+      <SalesChart seriesData={salesData?.series ?? []} />
     </div>
   );
 }
