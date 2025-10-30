291ec7859d803b6cc85621826f6b7b054eee8d8e
kwonboryong <kwonboryong@naver.com>
2025-10-30 20:28:19 +0900

    fix: 소수점 제거

--- src/components/page/sales-chart/SalesKPICard.tsx
+++ src/components/page/sales-chart/SalesKPICard.tsx
@@ -10,7 +10,9 @@ export default function SalesKPICard({ title, value }: Props) {
     <div className="flex flex-col gap-2 p-5 border rounded-md bg-white">
       <p className="text-gray-600">{title}</p>
       <div className="flex items-baseline gap-2">
-        <p className="text-2xl font-bold">{value.toLocaleString()}</p>
+        <p className="text-2xl font-bold">
+          {Math.floor(value).toLocaleString()}
+        </p>
         <span className="text-gray-500">원</span>
       </div>
     </div>
