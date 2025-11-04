7df32c023d599fa49137d5eb2b6bfabc0a361ca1
kwonboryong <kwonboryong@naver.com>
2025-11-04 12:52:31 +0900

    feat: Zod를 활용한 menu API 데이터 검증 구현

--- src/app/api/menu.ts
+++ src/app/api/menu.ts
@@ -1,7 +1,9 @@
 import { instance } from '@/lib/axios';
-import { MenuItem } from '@/lib/schemas/menu';
+import { MenuListSchema } from '@/lib/schemas/menu';
+import { validate } from '@/app/api/validate';
 
-export async function getMenus(): Promise<MenuItem[]> {
-  const { data } = await instance.get<MenuItem[]>(`/stores/@me/menus`);
-  return data;
+export async function getMenus() {
+  const { data } = await instance.get(`/stores/@me/menus`);
+
+  return validate(data, MenuListSchema);
 }
