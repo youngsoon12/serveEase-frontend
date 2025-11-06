41b3dfc06b3481b629973ee5e2021aadb7a2790c
youngsoon12 <enejwk789@gmail.com>
2025-11-06 11:32:38 +0900

    feat: logout API, useLogout 추가

--- src/hooks/useLogin.ts
+++ src/hooks/useLogin.ts
@@ -1,8 +1,32 @@
 import { useMutation } from '@tanstack/react-query';
-import { postLogin, LoginRequest, LoginResponse } from '@/app/api/login';
+import {
+  postLogin,
+  postLogout,
+  LoginRequest,
+  LoginResponse,
+} from '@/app/api/login';
 
-export default function useLogin() {
+export function useLogin() {
   return useMutation<LoginResponse, Error, LoginRequest>({
     mutationFn: (body: LoginRequest) => postLogin(body),
+  });
+}
+
+export function useLogout() {
+  return useMutation({
+    mutationFn: () => postLogout(),
+    onSuccess: () => {
+      localStorage.removeItem('storeId');
+      localStorage.removeItem('storeName');
+
+      const cookies = ['accessToken', 'isLoggedIn', 'storeId', 'storeName'];
+      cookies.forEach((cookieName) => {
+        document.cookie = `${cookieName}=; Max-Age=0; path=/;`;
+        document.cookie = `${cookieName}=; expires=Thu, 01 Jan 1970 00:00:00 GMT; path=/;`;
+      });
+    },
+    onError: (error) => {
+      console.error('로그아웃 실패:', error);
+    },
   });
 }
